985145e1998bf605ea34038d42d4f1a1fd7da9e6
saarm24 <44257528+saarm24@users.noreply.github.com>
2020-11-07 16:50:29 +0200

    Add files via upload

---
+++ project/Utilities/ServerInfo.java
@@ -0,0 +1,15 @@
+package com.Project.project.Utilities;
+
+public class ServerInfo {
+    public static String GetServerInfoID() {
+        return "";
+    }
+
+    public static String GetServerInfoKey() {
+        return "";
+    }
+
+    public static String GetServerInfoLink() {
+        return "";
+    }
+}
